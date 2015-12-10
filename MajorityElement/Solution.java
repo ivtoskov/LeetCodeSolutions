eb53c791747be6a1939eee08cedeeaa06f09d9b4
Ivaylo Toskov <kennelcrash@gmail.com>
2015-12-10 23:18:34 +0100

    Improved Majority Element

--- MajorityElement/Solution.java
+++ MajorityElement/Solution.java
@@ -1,12 +1,14 @@
 public class Solution {
     public int majorityElement(int[] nums) {
-        java.util.Map<Integer, Integer> counts = new java.util.HashMap<>();
+        int result = 0, count = 0;
         for(int i: nums) {
-            Integer current = counts.get(i);
-            if(current == null) current = 0;
-            if(current + 1 > nums.length / 2) return i;
-            counts.put(i, current + 1);
+            if(count == 0 || i == result) {
+                result = i;
+                ++ count;
+            } else {
+                --count;
+            }
         }
-        return -1;
+        return result;
     }
 }
