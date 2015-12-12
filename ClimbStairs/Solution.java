ef2d17fbfd00d02d9dbb99877f3ac53e85e82af7
Ivaylo Toskov <kennelcrash@gmail.com>
2015-12-12 16:04:49 +0100

    Made climb stairs minimalistic

--- ClimbStairs/Solution.java
+++ ClimbStairs/Solution.java
@@ -1,14 +1,12 @@
 public class Solution {
     public int climbStairs(int n) {
-        if(n <= 1) return n;
+        if(n <= 1) return 1;
         int prev1 = 1;
         int prev2 = 1;
-        int current = 2;
         for(int i = 2; i <= n; ++i) {
-            current = prev1 + prev2;
-            prev1 = current - prev1;
-            prev2 = current;
+            prev2 = prev2 + prev1;
+            prev1 = prev2 - prev1;
         }
-        return current;
+        return prev2;
     }
 }
