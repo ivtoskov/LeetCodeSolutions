a55f260216467ca5e40d44c31bf5222574aa7a5a
Ivaylo Toskov <kennelcrash@gmail.com>
2015-12-14 01:02:46 +0100

    Implemented BalancedBinaryTree

---
+++ BalancedBinaryTree/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return depth(root) != -1;
+    }
+    
+    private int depth(TreeNode node) {
+        if(node == null) return 0;
+        
+        int left = depth(node.left);
+        if(left == -1) return -1;
+        
+        int right = depth(node.right);
+        if(right == -1) return -1;
+        
+        if(Math.abs(right - left) > 1) return -1;
+        
+        return Math.max(left, right) + 1;
+    }
+}
