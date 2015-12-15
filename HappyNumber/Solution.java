def3d026ad97f33a29f8de1cac1f65e4109ec18f
Ivaylo Toskov <kennelcrash@gmail.com>
2015-12-15 14:05:39 +0100

    Implemented HappyNumber

---
+++ HappyNumber/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean isHappy(int n) {
+        while(true) {
+            if(n < 10) break;
+            int newn = 0;
+            while(n != 0) {
+                int digit = n % 10;
+                n /= 10;
+                newn += digit * digit;
+            }
+            n = newn;
+        }
+        return n == 1 || n == 7;
+    }
+}
