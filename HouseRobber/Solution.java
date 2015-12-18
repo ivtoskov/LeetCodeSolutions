f9b0139fea6effe66a21275a3038cdf00f510f9a
Ivaylo Toskov <kennelcrash@gmail.com>
2015-12-18 01:56:46 +0100

    Implemented HouseRobber

---
+++ HouseRobber/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int rob(int[] nums) {
+        int[] cache = new int[nums.length];
+        for(int i = 0; i < cache.length; ++i) {
+            cache[i] = -1;
+        }
+        return rob(nums, 0, cache);
+    }
+    
+    public int rob(int[] nums, int index, int[] cache) {
+        if(index >= nums.length) return 0;
+        if(cache[index] >= 0) return cache[index];
+        int sol1 = rob(nums, index + 1, cache);
+        int sol2 = nums[index] + rob(nums, index + 2, cache);
+        cache[index] = Math.max(sol1, sol2);
+        return cache[index];
+    }
+}
