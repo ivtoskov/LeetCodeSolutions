42dd2254cbb4e7a7a6fedb970819973521a0d468
Ivaylo Toskov <kennelcrash@gmail.com>
2015-12-28 22:43:08 +0100

    Implemented LengthOfLastWord

---
+++ LengthOfLastWord/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int answer = 0;
+        int index = s.length() - 1;
+        while(index >= 0 && s.charAt(index) == ' ') --index;
+        while(index >= 0) {
+            if(s.charAt(index--) == ' ') break;
+            ++answer;
+        }
+        return answer;
+    }
+}
