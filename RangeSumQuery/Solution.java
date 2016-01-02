6846c40a24414a4a9f15c9cf6a053a26342b0f46
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-02 19:48:17 +0100

    Implemented RangeSumQuery

---
+++ RangeSumQuery/Solution.java
@@ -0,0 +1,18 @@
+public class NumArray {
+    int[] sums;
+    int[] nums;
+    public NumArray(int[] nums) {
+        if(nums != null && nums.length != 0) {
+            sums = new int[nums.length];
+            sums[0] = nums[0];
+            for(int i = 1; i < nums.length; ++i) {
+                sums[i] = nums[i] + sums[i - 1];
+            }
+        }
+        this.nums = nums;
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j] - sums[i] + nums[i];
+    }
+}
