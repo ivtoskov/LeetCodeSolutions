6c8b76d2f224a767b282ad67a483db26e903e888
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-02 16:08:06 +0100

    Implemented ValidParentheses

---
+++ ValidParentheses/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()) {
+            if(c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if(stack.empty()) return false;
+                char left = stack.pop();
+                if(c == ')' && left != '(') return false;
+                else if(c == '}' && left != '{') return false;
+                else if(c == ']' && left != '[') return false;
+            }
+        }
+        return stack.empty();
+    }
+}
