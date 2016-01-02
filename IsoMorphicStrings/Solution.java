03409897add1e2ea345922e9776cacd7eb085ac5
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-02 16:23:49 +0100

    Implemented IsoMorphicStrings

---
+++ IsoMorphicStrings/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length()) return false;
+        Map<Character, Character> map = new HashMap<>();
+        for(int i = 0; i < s.length(); ++i) {
+            char schar = s.charAt(i);
+            char tchar = t.charAt(i);
+            if(!map.containsKey(schar) && !map.containsValue(tchar)) map.put(schar, tchar);
+            else if(!map.containsKey(schar) || map.get(schar) != tchar) return false;
+        }
+        return true;
+    }
+}
