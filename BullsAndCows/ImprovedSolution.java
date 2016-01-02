455bf7c043854adc1bcc8606a978d9ba9f65e75a
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-02 17:28:25 +0100

    Improved BullsAndCows

---
+++ BullsAndCows/ImprovedSolution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public String getHint(String secret, String guess) {
+        int[] sDigits = new int[10];
+        int[] gDigits = new int[10];
+        int bulls = 0, cows = 0;
+        for(int i = 0; i < secret.length(); ++i) {
+            if(secret.charAt(i) == guess.charAt(i)) ++bulls;
+            else {
+                sDigits[secret.charAt(i) - '0']++;
+                gDigits[guess.charAt(i) - '0']++;
+            }
+        }
+        for(int i = 0; i < 10; ++i) {
+            cows += Math.min(sDigits[i], gDigits[i]);
+        }
+        return bulls + "A" + cows + "B";
+    }
+}
