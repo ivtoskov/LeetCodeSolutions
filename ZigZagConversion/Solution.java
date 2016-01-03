74f5f5b1582a936e72c351ba1a08d2a344a8fc2e
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-03 20:10:07 +0100

    Improved ZigZagConversion

--- ZigZagConversion/Solution.java
+++ ZigZagConversion/Solution.java
@@ -2,11 +2,10 @@ public class Solution {
     public String convert(String s, int numRows) {
         if(numRows < 2 || s.length() < numRows) return s;
         char[] converted = new char[s.length()];
-        int step = (numRows - 2) * 2 + 2, index = 0;
+        int step = (numRows - 1) << 1, index = 0;
         for(int i = 0; i <= numRows - 1; ++i) {
-            for(int j = i; j < s.length(); j += step) {
+            for(int j = i, addChar = step - i; j < s.length(); j += step, addChar += step) {
                 converted[index++] = s.charAt(j);
-                int addChar = j + step - i * 2;
                 if(i % (numRows - 1) != 0 && addChar < s.length()) converted[index++] = s.charAt(addChar);
             }
         }
