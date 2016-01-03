28f2fc63ecf449ce146ef571b25144b30501b97b
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-03 17:11:38 +0100

    Implemented ImplementStrStrStr

---
+++ ImplementStrStrStr/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        int hlength = haystack.length();
+        int nlength = needle.length();
+        for(int i = 0; i <= hlength - nlength; ++i) {
+            String sub = haystack.substring(i, i + nlength);
+            if(needle.equals(sub)) return i;
+        }
+        return -1;
+    }
+}
