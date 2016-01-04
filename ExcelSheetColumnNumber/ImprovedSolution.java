ecf1a23e2f36cb332635366732820bf9d771f493
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-04 17:36:43 +0100

    ExcelSheetColumnNumber

---
+++ ExcelSheetColumnNumber/ImprovedSolution.java
@@ -0,0 +1,7 @@
+public class ImprovedSolution {
+    public int titleToNumber(String s) {
+        int answer = 0;
+        for(char c: s.toCharArray()) answer = answer * 26 + (c - 'A' + 1);
+        return answer;
+    }
+}
