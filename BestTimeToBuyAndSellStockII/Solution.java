e11065b5498be1ae55a4144ebdd8ae62e5aa1cb3
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-09 22:28:13 +0100

    Implemented BestTimeToBuyAndSellStockII

---
+++ BestTimeToBuyAndSellStockII/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length < 1) {
+            return 0;
+        }
+        int profit = 0, min = prices[0];
+        for(int i = 1; i < prices.length; ++i) {
+            if(prices[i] < prices[i-1]) {
+                profit += prices[i - 1] - min;
+                min = prices[i];
+            }
+        }
+        return profit + prices[prices.length - 1] - min;
+    }
+}
