8e200d89ae61917adca31c3b4977b1fe14d4d41a
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-14 12:31:12 +0100

    Implemented UniqueBinarySearchTrees

---
+++ UniqueBinarySearchTrees/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int numTrees(int n) {
+        if(n == 0) {
+            return 1;
+        }
+        int[] trees = new int[n + 1];
+        trees[0] = 1;
+        trees[1] = 1;
+        for(int i = 2; i <= n; ++i) {
+            for(int j = 1; j <= i; ++j) {
+                trees[i] += trees[j - 1] * trees[i - j];
+            }
+        }
+        return trees[n];
+    }
+}
