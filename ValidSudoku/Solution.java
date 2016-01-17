4ce46bbc4be84950c23bb73be8dd878de45eb3d9
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-17 01:04:16 +0100

    Improved formatting of ValidSudoku

--- ValidSudoku/Solution.java
+++ ValidSudoku/Solution.java
@@ -9,7 +9,6 @@ public class Solution {
             }
         }
         
-        
         for(int i = 0; i < 3; ++i) {
             for(int j = 0; j < 3; ++j) {
                 int[] square = new int[10];
