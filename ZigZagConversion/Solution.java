837895b9655d62341208140a33ed740585120b65
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-27 23:04:15 +0100

    Improved formatting of ZigZagConversion

--- ZigZagConversion/Solution.java
+++ ZigZagConversion/Solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public String convert(String s, int numRows) {
         if(numRows < 2 || s.length() < numRows) return s;
         char[] converted = new char[s.length()];
-        int step = (numRows - 1) << 1, index = 0;
+        int index = 0, step = (numRows - 1) << 1;
         for(int i = 0; i <= numRows - 1; ++i) {
             for(int j = i, addChar = step - i; j < s.length(); j += step, addChar += step) {
                 converted[index++] = s.charAt(j);
