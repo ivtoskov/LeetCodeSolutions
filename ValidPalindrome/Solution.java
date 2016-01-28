1c72b940ba4251a4ea6cc107744fc1e64328a183
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-28 22:43:39 +0100

    Improved formatting of ValidPalindrome

--- ValidPalindrome/Solution.java
+++ ValidPalindrome/Solution.java
@@ -1,8 +1,9 @@
 public class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-        for(int i = 0; i < s.length() / 2; ++i)
+        for(int i = 0; i < s.length() / 2; ++i) {
             if(s.charAt(i) != s.charAt(s.length() - i - 1)) return false;
+        }
         return true;
     }
 }
