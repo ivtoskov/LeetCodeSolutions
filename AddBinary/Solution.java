ab05661a203513e00ecdf66609e87d6daa8c4d80
Ivaylo Toskov <kennelcrash@gmail.com>
2016-01-30 23:13:09 +0100

    Improved the solution of AddBinary

--- AddBinary/Solution.java
+++ AddBinary/Solution.java
@@ -4,13 +4,12 @@ public class Solution {
         int indexB = b.length() - 1;
         int carry = 0;
         StringBuilder sb = new StringBuilder();
-        while(indexA >= 0 || indexB >= 0) {
+        while(indexA >= 0 || indexB >= 0 || carry > 0) {
             int bitA = (indexA >= 0 ? a.charAt(indexA--) : '0') - '0';
             int bitB = (indexB >= 0 ? b.charAt(indexB--) : '0') - '0';
             sb.insert(0, (bitA + bitB + carry) % 2);
             carry = (bitA + bitB + carry) > 1 ? 1 : 0;
         }
-        if(carry == 1) sb.insert(0, 1);
         return sb.toString();
     }
 }
