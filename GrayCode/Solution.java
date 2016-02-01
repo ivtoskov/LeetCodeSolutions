d2e1a46c18bbe28e10b93cd2cab1a0bd0949ef53
Ivaylo Toskov <kennelcrash@gmail.com>
2016-02-01 22:50:29 +0100

    Improved formatting of GrayCode

--- GrayCode/Solution.java
+++ GrayCode/Solution.java
@@ -1,13 +1,11 @@
 public class Solution {
     public List<Integer> grayCode(int n) {
-        // Initialize arraylist
         List<Integer> codes = new ArrayList<>((int)Math.pow(2, n));
         codes.add(0, 0);
         if(n == 0) {
             return codes;
         }
         
-        // Compute recursively
         for(int i = 0, index = 1; i < n; ++i) {
             int mask = (1 << i);
             for(int j = index - 1; j >= 0; --j) {
