abc0bf19c5665b386f4193dad84c0b23dea2375b
Ivaylo Toskov <kennelcrash@gmail.com>
2016-02-02 18:04:38 +0100

    Implemented GenerateMatrix

---
+++ GenerateMatrix/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        int upperBound = 0, lowerBound = n - 1, leftBound = 0, rightBound = n - 1, num = 1;
+        while(upperBound <= lowerBound) {
+            for(int i = upperBound, j = leftBound; result[i][j] == 0; ++num) {
+                result[i][j] = num;
+                if(i == upperBound && j < rightBound) ++j;
+                else if(i < lowerBound && j == rightBound) ++i;
+                else if(i == lowerBound && j > leftBound) --j;
+                else if(i > upperBound) --i;
+            }
+            
+            ++upperBound;
+            --lowerBound;
+            ++leftBound;
+            --rightBound;
+        }
+        
+        return result;
+    }
+}
