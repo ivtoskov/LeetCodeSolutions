bc27432dfbf7d610f79f81a3d455adb732cda7b4
Ivaylo Toskov <kennelcrash@gmail.com>
2016-02-02 17:24:17 +0100

    Implemented OddEvenLinkedList

---
+++ OddEvenLinkedList/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode odd = head, even = head.next, evenHead = head.next, next = head.next.next;
+        while(next != null) {
+            odd.next = next;
+            even.next = next.next;
+            odd = odd.next;
+            even = even.next;
+            
+            if(next.next != null) {
+                next = next.next.next;
+            } else {
+                next = next.next;
+            }
+        }
+        odd.next = evenHead;
+        
+        return head;
+    }
+}
