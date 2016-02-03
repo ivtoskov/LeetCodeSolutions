1f4dacd0a7699953b60823173139f1f0e91250e0
Ivaylo Toskov <kennelcrash@gmail.com>
2016-02-03 19:06:50 +0100

    Implemented VerifyPreorderSerializationOfABinaryTree

---
+++ VerifyPreorderSerializationOfABinaryTree/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean isValidSerialization(String preorder) {
+        String[] nodes = preorder.split(",");
+        int number = 1;
+        for(int i = 0; i < nodes.length; ++i) {
+            if(number == 0) return false;
+            
+            if("#".equals(nodes[i])) --number;
+            else ++number;
+        }
+        return number == 0;
+    }
+}
