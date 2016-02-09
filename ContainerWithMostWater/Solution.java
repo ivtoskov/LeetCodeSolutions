4bac032b2fcf4679b080b61533f0f40c70a5aff0
Ivaylo Toskov <kennelcrash@gmail.com>
2016-02-09 18:21:30 +0100

    Implemented ContainerWithMostWater

---
+++ ContainerWithMostWater/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1, maxx = 0, leftMax, rightMax;
+        while(i < j) {
+            leftMax = height[i];
+            rightMax = height[j];
+            maxx = Math.max(maxx, (j - i) * Math.min(leftMax, rightMax));
+            
+            if(leftMax < rightMax)
+                while(i < j && height[i] <= leftMax) ++i;
+            else
+                while(i < j && height[j] <= rightMax) --j;
+        }
+        return maxx;
+    }
+}
