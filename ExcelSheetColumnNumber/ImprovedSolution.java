b4bdf5c8775387301f3ea522dc32e918cc06ab67
Ivaylo Toskov <kennelcrash@gmail.com>
2016-02-20 00:02:30 +0100

    Improved the formatting of ExcelSheetColumnNumber

--- ExcelSheetColumnNumber/ImprovedSolution.java
+++ ExcelSheetColumnNumber/ImprovedSolution.java
@@ -1,7 +1,9 @@
 public class ImprovedSolution {
     public int titleToNumber(String s) {
         int answer = 0;
-        for(char c: s.toCharArray()) answer = answer * 26 + (c - 'A' + 1);
+        for(char c: s.toCharArray()) {
+            answer = answer * 26 + (c - 'A' + 1);
+        }
         return answer;
     }
 }
