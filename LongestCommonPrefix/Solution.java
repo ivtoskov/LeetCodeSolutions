bcc366657e2d724a7e902aa4b3f76500180775d0
Ivaylo Toskov <kennelcrash@gmail.com>
2016-02-25 00:27:10 +0100

    Improved the formatting of LongestCommonPrefix

--- LongestCommonPrefix/Solution.java
+++ LongestCommonPrefix/Solution.java
@@ -4,7 +4,9 @@ public class Solution {
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < strs[0].length(); ++i) {
             for(String s: strs) {
-                if(i >= s.length() || strs[0].charAt(i) != s.charAt(i)) return sb.toString();
+                if(i >= s.length() || strs[0].charAt(i) != s.charAt(i)) {
+					     return sb.toString();
+                }
             }
             sb.append(strs[0].charAt(i));
         }
