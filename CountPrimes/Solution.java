d189f0011b05bb12eb7696147085125c28222d7e
Ivaylo Toskov <kennelcrash@gmail.com>
2016-02-27 19:39:03 +0100

    Improved the formatting of CountPrimes

--- CountPrimes/Solution.java
+++ CountPrimes/Solution.java
@@ -12,7 +12,9 @@ public class Solution {
         }
         int count = 0;
         for (int i = 2; i < n; i++) {
-           if (isPrime[i]) count++;
+           if (isPrime[i]) {
+              count++;
+           }
         }
         return count;
     }
