f82f88b4269a7022e5454027ce296e2532928814
Ivaylo Toskov <kennelcrash@gmail.com>
2016-03-02 23:52:53 +0100

    Improved the formatting of ContainsDuplicate

--- ContainsDuplicate/Solution.java
+++ ContainsDuplicate/Solution.java
@@ -3,7 +3,9 @@ public class Solution {
         if(nums == null || nums.length == 1) return false;
         java.util.Set<Integer> prev = new java.util.HashSet<>();
         for(int i: nums) {
-            if(!prev.add(i)) return true;
+            if(!prev.add(i)) {
+                return true;
+            }
         }
         return false;
     }
