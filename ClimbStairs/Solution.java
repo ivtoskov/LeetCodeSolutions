7805fed6008ab95d1465bbe14896c9de5f68202d
Ivaylo Toskov <kennelcrash@gmail.com>
2016-03-12 02:11:48 +0100

    Improved the formatting of ClimbStairs

--- ClimbStairs/Solution.java
+++ ClimbStairs/Solution.java
@@ -1,6 +1,8 @@
 public class Solution {
     public int climbStairs(int n) {
-        if(n <= 1) return 1;
+        if(n <= 1) {
+		      return 1;
+        }
         int prev1 = 1;
         int prev2 = 1;
         for(int i = 2; i <= n; ++i) {
