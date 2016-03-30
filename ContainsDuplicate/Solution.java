f2dd0dd6eff1570f77bb2788a012415a159aeeb3
Ivaylo Toskov <kennelcrash@gmail.com>
2016-03-30 20:28:30 +0200

    Improved the formatting of ContainsDuplicate.

--- ContainsDuplicate/Solution.java
+++ ContainsDuplicate/Solution.java
@@ -1,6 +1,8 @@
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums == null || nums.length == 1) return false;
+        if(nums == null || nums.length == 1) {
+            return false;
+        }
         java.util.Set<Integer> prev = new java.util.HashSet<>();
         for(int i: nums) {
             if(!prev.add(i)) {
