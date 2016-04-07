fffc94c5d6c6917bd0662cbccba97f9b6a22ba7d
Ivaylo Toskov <kennelcrash@gmail.com>
2016-04-07 15:34:01 +0200

    Improved the formatting of WordPattern.

--- WordPattern/Solution.java
+++ WordPattern/Solution.java
@@ -2,7 +2,9 @@ public class Solution {
     public boolean wordPattern(String pattern, String str) {
         String[] words = str.split(" ");
         Map<Character, String> map = new HashMap<>();
-        if(pattern.length() != words.length) return false;
+        if(pattern.length() != words.length) {
+            return false;
+        }
         for(int i = 0; i < pattern.length(); ++i) {
             if(!map.containsKey(pattern.charAt(i)) && !map.containsValue(words[i])) 
                 map.put(pattern.charAt(i), words[i]);
