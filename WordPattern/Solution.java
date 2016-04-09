9d5f1e53a45d6142a9706e2f6e35b8a71deee657
Ivaylo Toskov <kennelcrash@gmail.com>
2016-04-09 16:28:25 +0200

    Improved the formatting of WordPattern.

--- WordPattern/Solution.java
+++ WordPattern/Solution.java
@@ -6,8 +6,9 @@ public class Solution {
             return false;
         }
         for(int i = 0; i < pattern.length(); ++i) {
-            if(!map.containsKey(pattern.charAt(i)) && !map.containsValue(words[i])) 
+            if(!map.containsKey(pattern.charAt(i)) && !map.containsValue(words[i])) {
                 map.put(pattern.charAt(i), words[i]);
+            }
             else
                 if(!map.containsKey(pattern.charAt(i)) || !map.get(pattern.charAt(i)).equals(words[i])) return false;
         }
