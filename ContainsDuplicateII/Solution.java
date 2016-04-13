d76a1d27899fd1daef8414f21793415ed3c29254
Ivaylo Toskov <kennelcrash@gmail.com>
2016-04-13 14:27:25 +0200

    Improved the formatting of ContainsDuplicate.

--- ContainsDuplicateII/Solution.java
+++ ContainsDuplicateII/Solution.java
@@ -2,7 +2,9 @@ public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; ++i) {
-            if(map.containsKey(nums[i]) && (i - map.get(nums[i])) <= k) return true;
+            if(map.containsKey(nums[i]) && (i - map.get(nums[i])) <= k) {
+                return true;
+            }
             map.put(nums[i], i);
         }
         return false;
