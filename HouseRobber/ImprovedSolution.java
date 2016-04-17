778556d688cee8e42883d4f73fc5df0940135221
Ivaylo Toskov <kennelcrash@gmail.com>
2016-04-17 18:03:56 +0200

    Improved the formatting of HouseRobber.

--- HouseRobber/ImprovedSolution.java
+++ HouseRobber/ImprovedSolution.java
@@ -1,6 +1,8 @@
 public class ImprovedSolution {
     public int rob(int[] nums) {
-        if(nums == null) return 0;
+        if(nums == null) {
+            return 0;
+        }
         
         int taken = 0, notTaken = 0, temp = 0;
         for(int i = 0; i < nums.length; ++i) {
