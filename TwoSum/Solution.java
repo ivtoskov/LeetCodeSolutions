ac9067c7bb8feb21fbdee307131b94473074cb33
Ivaylo Toskov <kennelcrash@gmail.com>
2016-04-21 16:45:45 +0200

    Implemented TwoSum.

---
+++ TwoSum/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> prev = new HashMap<>();
+        for(int i = 0; i < nums.length; ++i) {
+            if(prev.containsKey(target-nums[i])) {
+                return new int[]{prev.get(target-nums[i]), i};
+            }
+            prev.put(nums[i], i);
+        }
+        return null;
+    }
+}
