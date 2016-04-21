80ad5226aa1b54becbe1738ba7059949f6f3a539
Ivaylo Toskov <kennelcrash@gmail.com>
2016-04-21 16:20:49 +0200

    Implemented PowerOfFour.

---
+++ PowerOfFour/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        if(num < 1) {
+            return false;
+        }
+        
+        while(num % 4 == 0) {
+            num /= 4;
+        }
+        
+        return num == 1;
+    }
+}
