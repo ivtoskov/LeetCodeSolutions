596954714f58d33e06ae485d01cfa29bce8e7368
Ivaylo Toskov <kennelcrash@gmail.com>
2016-04-21 17:27:00 +0200

    Improved CountingBits.

--- CountingBits/Solution.java
+++ CountingBits/Solution.java
@@ -1,19 +1,15 @@
 public class Solution {
     public int[] countBits(int num) {
-        if(num < 1) {
-            return new int[] {0};
-        }
         int[] result = new int[num + 1];
         result[0] = 0;
-        result[1] = 1;
         
-        int stride = 2, index = 2;
+        int stride = 1, index = 1;
         while(index <= num) {
             for(int i = 0; i < stride && index <= num; ++i) {
                 result[index] = 1 + result[index - stride];
                 ++index;
             }
-            stride *= 2;
+            stride <<= 1;
         }
         
         return result;
