c35a58c12cb54f7cdae631b16191e21b14d44552
Ivaylo Toskov <kennelcrash@gmail.com>
2016-04-23 23:05:41 +0200

    Implemented IntegerBreak.

---
+++ IntegerBreak/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int integerBreak(int n) {
+        if(n <= 3) {
+            return n - 1;
+        }
+        return n % 3 == 0 ? (int) Math.pow(3, n / 3) : (n % 3 == 1 ? (int) Math.pow(3, n / 3 - 1) * 4 : (int) Math.pow(3, n / 3) * 2);
+    }
+}
