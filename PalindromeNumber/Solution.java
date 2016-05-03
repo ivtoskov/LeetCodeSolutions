2e7f3ccd125d3f47267c6988eac5eb53aee07028
Ivaylo Toskov <kennelcrash@gmail.com>
2016-05-03 23:00:17 +0200

    Implemented PalindromeNumber.

--- PalindromeNumber/Solution.java
+++ PalindromeNumber/Solution.java
@@ -1,6 +1,8 @@
 public class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0) return false;
+        if(x < 0) {
+            return false;
+        }
         int sum = 0, target = x;
         while(x != 0) {
             int temp = x % 10;
