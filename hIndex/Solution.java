f49d1ca7138ad46b4af9bd37d87ec82fab69f6ed
Ivaylo Toskov <kennelcrash@gmail.com>
2016-05-07 13:44:07 +0200

    Implemented hIndex.

---
+++ hIndex/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int hIndex(int[] citations) {
+        int low = 0, high = citations.length;
+        while(low < high - 1) {
+            int mid = low + (high - low) / 2;
+            if(check(citations, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        while(!check(citations, high)) --high;
+        return high;
+    }
+    
+    private boolean check(int[] citations, int h) {
+        int sat = 0;
+        for(int cit: citations) {
+            if(cit >= h) {
+                ++sat;
+            }
+        }
+        return sat >= h;
+    }
+}
