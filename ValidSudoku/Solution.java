eb64e51b7a6241dde3b32619d621d9f8c3597bf6
Ivaylo Toskov <kennelcrash@gmail.com>
2016-05-10 15:48:03 +0200

    Improved the formatting of ValidSudoku.

--- ValidSudoku/Solution.java
+++ ValidSudoku/Solution.java
@@ -4,7 +4,9 @@ public class Solution {
             int[] row = new int[10];
             int[] column = new int[10];
             for(int j = 0; j < 9; ++j) {
-                if(board[i][j] != '.' && row[board[i][j] - '0']++ > 0) return false;
+                if(board[i][j] != '.' && row[board[i][j] - '0']++ > 0) {
+                    return false;
+                }
                 if(board[j][i] != '.' && column[board[j][i] - '0']++ > 0) return false;
             }
         }
