e70c15325825c049bad93e460492648661173fc7
Ivaylo Toskov <kennelcrash@gmail.com>
2016-08-27 17:41:08 +0200

    Implemented CombinationSumIV

---
+++ CombinationSumIV/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        Arrays.sort(nums);
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; ++i) {
+            for (int num: nums) {
+                if (num > i) {
+                    break;
+                }
+                dp[i] += dp[i - num];
+            }
+        }
+        return dp[target];
+    }
+}
