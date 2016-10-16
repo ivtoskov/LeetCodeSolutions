314ac8f35199dbcd9ce96673eb3192d0d26eb47e
Ivaylo Toskov <kennelcrash@gmail.com>
2016-10-16 02:59:19 +0200

    Implemented WordSearchII.

---
+++ WordSearchII/Solution.java
@@ -0,0 +1,52 @@
+public class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> result = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[i].length; ++j) {
+                dfs(board, i, j, root, result);
+            }
+        }
+        return result;
+    }
+    
+    private void dfs(char[][] board, int i, int j, TrieNode node, List<String> result) {
+        if (i < 0 || board.length <= i || j < 0 || board[i].length <= j || board[i][j] == '#' || node.children[board[i][j] - 'a'] == null) {
+            return;
+        }
+        
+        char c = board[i][j];
+        node = node.children[c - 'a'];
+        if (node.word != null) {
+            result.add(node.word);
+            node.word = null;
+        }
+        board[i][j] = '#';
+        dfs(board, i + 1, j, node, result);
+        dfs(board, i - 1, j, node, result);
+        dfs(board, i, j + 1, node, result);
+        dfs(board, i, j - 1, node, result);
+        board[i][j] = c;
+    }
+    
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String word : words) {
+            TrieNode cur = root;
+            for (char c : word.toCharArray()) {
+                int index = c - 'a';
+                if (cur.children[index] == null) {
+                    cur.children[index] = new TrieNode();
+                }
+                cur = cur.children[index];
+            }
+            cur.word = word;
+        }
+        return root;
+    }
+    
+    private class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word;
+    }
+}
