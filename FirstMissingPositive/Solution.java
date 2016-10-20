2e61b6349ade8ad16de9a3d0e1e910784d156ab7
Ivaylo Toskov <kennelcrash@gmail.com>
2016-10-20 15:25:05 +0200

    Implemented FirstMissingPositive.

---
+++ FirstMissingPositive/Solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length;) {
+            if (nums[i] <= 0 || nums[i] > nums.length || nums[i] == i + 1) {
+                ++i;
+            } else if (nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            } else {
+                ++i;
+            }
+        }
+        
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
