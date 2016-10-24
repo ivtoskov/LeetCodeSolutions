1e1b504e454295f1f8ef063f4deb992d3736e93d
Ivaylo Toskov <kennelcrash@gmail.com>
2016-10-24 23:00:28 +0200

    Implemented TrappingRainWater.

---
+++ TrappingRainWater/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1, maxLeft = 0, maxRight = 0, res = 0;
+        
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    res += maxLeft - height[left];
+                }
+                ++left;
+            } else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    res += maxRight - height[right];
+                }
+                --right;
+            }
+        }
+        
+        return res;
+    }
+}
